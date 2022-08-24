ca81ddb612972d343e1a84471527f0d66f1cc7a1
ninjasockmonkey <kevin@kevindibble.com>
2022-08-23 19:14:22 -0700

    create classes for Major/Minor Arcanas and Spreads

---
+++ tarot.py
@@ -0,0 +1,17 @@
+# A tarot console reader by Kevin Dibble
+# contact: kevin@kevindibble.com
+
+import random
+from unicodedata import name
+
+class MajorArcana(self,name,description):
+    self.name = name
+    self.description = description
+
+class MinorArcana(self,number,suit):
+    self.number = number
+    self.suit - suite
+
+class Spread(self,name,number_of_cards):
+    self.name = name
+    self.number_of_cards = number_of_cards
